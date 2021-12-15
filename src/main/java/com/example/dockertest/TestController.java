408ff586337e072b0967e4a5ce7cf12247b52f23
김지은 <kimmjieun@gmail.com>
2021-12-15 17:12:33 +0900

    sout 추가

--- src/main/java/com/example/dockertest/TestController.java
+++ src/main/java/com/example/dockertest/TestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/")
     public String hello(){
+        System.out.println("** test **");
         return "Hello Spring Boot";
     }
 
